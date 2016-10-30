fbfd945d960b52448eab6bc73f151c5fff750393
Rishabh Bhardwaj <brishabh@iitk.ac.in>
2016-10-30 21:38:15 +0000

    Added display links to the admin interface

--- project/posts/admin.py
+++ project/posts/admin.py
@@ -4,6 +4,8 @@ from django.contrib import admin
 from .models import Post
 
 class PostModelAdmin(admin.ModelAdmin):
+	list_display = ["title", "updated"]
+	list_display_links = ["updated"]
 	class Meta:
 		model = Post
 
