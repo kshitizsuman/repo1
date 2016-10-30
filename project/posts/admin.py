b3b658c6f4e01250af0cc9b10567677548e624c9
Rishabh Bhardwaj <brishabh@iitk.ac.in>
2016-10-30 21:39:05 +0000

    Timestamp added to the display

--- project/posts/admin.py
+++ project/posts/admin.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 from .models import Post
 
 class PostModelAdmin(admin.ModelAdmin):
-	list_display = ["title", "updated"]
+	list_display = ["title", "updated", "timestamp"]
 	list_display_links = ["updated"]
 	class Meta:
 		model = Post
