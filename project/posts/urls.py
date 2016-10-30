01a056df9252d1f802fa0e5a290b1ec178a8cd25
KSHITIZ SUMAN <kshitizsuman@gmail.com>
2016-10-30 23:28:15 +0000

    Mapped the functions to url

--- project/posts/urls.py
+++ project/posts/urls.py
@@ -2,5 +2,9 @@ from django.conf.urls import url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^$', "posts.views.post_home"),
+    url(r'^create/$', "posts.views.post_create"),
+    url(r'^detail/$', "posts.views.post_detail"),
+    url(r'^list/$', "posts.views.post_list"),
+    url(r'^update/$', "posts.views.post_update"),
+    url(r'^create/$', "posts.views.post_create"),
 ]
