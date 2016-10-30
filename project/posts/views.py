ef19413d82484df9aca3e14acaf8fbf3bd202466
Kshitiz Suman <kshitizsuman@gmail.com>
2016-10-31 04:54:43 +0530

    mapped the functions to url

--- project/posts/views.py
+++ project/posts/views.py
@@ -3,5 +3,17 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def post_home(request):
-	return HttpResponse("<h1>This is my first view.")
+def post_create(request):
+	return HttpResponse("<h1>Create")
+
+def post_detail(request):
+	return HttpResponse("<h1>Post detail")
+
+def post_list(request):
+	return HttpResponse("<h1>List view.")
+
+def post_update(request):
+	return HttpResponse("<h1>Update Post.")
+
+def post_delete(request):
+	return HttpResponse("<h1>Delete Post")
