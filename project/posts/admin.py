a4d680d0a949a78af29dedc6e3a21a876f951bde
Rishabh Bhardwaj <brishabh@iitk.ac.in>
2016-10-30 21:30:12 +0000

    customized the posts admin

--- project/posts/admin.py
+++ project/posts/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+
+from .models import Post
+
+class PostModelAdmin(admin.ModelAdmin):
+	class Meta:
+		model = Post
+
+
+admin.site.register(Post)
