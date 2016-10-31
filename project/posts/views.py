de66f87e0ba28090c507d858bb33a37c4434087a
Rishabh Bhardwaj <brishabh@iitk.ac.in>
2016-10-31 01:52:35 +0000

    modified the update function to edit the post

--- project/posts/views.py
+++ project/posts/views.py
@@ -1,16 +1,17 @@
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
 from django.shortcuts import render , get_object_or_404
 
 # Create your views here.
 from .forms import PostForm
 from .models import Post
+
 def post_create(request):
 	form = PostForm(request.POST or None)
 	if form.is_valid():
 		instance = form.save(commit=False)
 		print form.cleaned_data.get("title")
 		instance.save()
-
+		return HttpResponseRedirect(instance.get_absolute_url())
 	context ={
 		"form":form,
 	}
@@ -32,8 +33,20 @@ def post_list(request):
 	}
 	return render(request,"index.html",context )
 
-def post_update(request):
-	return HttpResponse("<h1>Update Post.")
+def post_update(request,id =None):
+	instance = get_object_or_404(Post, id=id)
+	form = PostForm(request.POST or None , instance=instance)
+	if form.is_valid():
+		instance = form.save(commit=False)
+		instance.save()
+		return HttpResponseRedirect(instance.get_absolute_url())
+	context ={
+		"title" : instance.title,
+		"instance" : instance,
+		"form":form,
+
+	}
+	return render(request,"post_form.html",context )
 
 def post_delete(request):
 	return HttpResponse("<h1>Delete Post")
