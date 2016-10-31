4f8c14ec305bc0f7401dfd28f9811e7f41af45ed
Kshitiz Suman <kshitizsuman@gmail.com>
2016-10-31 05:53:43 +0530

    Listed all the posts using the template

--- project/posts/views.py
+++ project/posts/views.py
@@ -13,7 +13,9 @@ def post_detail(request):
 	return render(request,"index.html",context )
 
 def post_list(request):
+	queryset = Post.objects.all()
 	context ={
+		"object_list" : queryset,
 		"title" : "List"
 	}
 	return render(request,"index.html",context )
