8b871c2dba8f8d561bd0de1c07c7da7b2c982d1c
Rishabh Bhardwaj <brishabh@iitk.ac.in>
2016-11-04 12:35:16 +0530

    deleted old database to add slug field

--- project/posts/models.py
+++ project/posts/models.py
@@ -2,13 +2,15 @@ from __future__ import unicode_literals
 
 from django.db import models
 from django.core.urlresolvers import reverse
-# Create your models here.
+from django.db.models.signals import pre_save¶
+
 
 def upload_location(instance, filename):
 	return "%s/%s" %(instance.id, filename)
 
 class Post(models.Model):
 	title = models.CharField(max_length=120)
+	#slug = models.SlugField(unique=True)
 	image = models.ImageField(upload_to=upload_location,
 		null=True, blank=True,
 		height_field="height_field",
