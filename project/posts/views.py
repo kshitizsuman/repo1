8587c1e7b65b408748065b42ce6fc7fb76a0e6ef
Anubhav Shrivaastava <anubhavs@iitk.ac.in>
2016-11-04 21:40:34 +0530

    added search feature

--- project/posts/views.py
+++ project/posts/views.py
@@ -1,6 +1,7 @@
 from urllib import quote_plus
 from django.contrib import messages
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.db.models import Q
 from django.shortcuts import render
 from django.http import HttpResponse,HttpResponseRedirect,Http404
 from django.shortcuts import render , get_object_or_404,redirect
@@ -43,6 +44,16 @@ def post_list(request):
 	queryset_list = Post.objects.active()
 	if request.user.is_staff or request.user.is_superuser:
 		query_list = Post.objects.all()
+
+	query = request.GET.get("q")
+	if query:
+		queryset_list = queryset_list.filter(
+			    Q(title__icontains=query)|
+			    Q(content__icontains=query)|
+			    Q(user__first_name__icontains=query)|
+			    Q(user__last_name__icontains=query)
+			    ).distinct()
+
 	paginator = Paginator(queryset_list, 2) # Show 25 contacts per page
 	page_request_var = "page"
 	page = request.GET.get(page_request_var)
