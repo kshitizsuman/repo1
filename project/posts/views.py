0151443f3aea82ab9f9fee700862ca35c68ad233
Rishabh Bhardwaj <brishabh@iitk.ac.in>
2016-11-04 12:04:23 +0530

    added image field

--- project/posts/views.py
+++ project/posts/views.py
@@ -9,7 +9,7 @@ from .forms import PostForm
 from .models import Post
 
 def post_create(request):
-	form = PostForm(request.POST or None)
+	form = PostForm(request.POST or None, request.FILES or None)
 	if form.is_valid():
 		instance = form.save(commit=False)
 		print form.cleaned_data.get("title")
@@ -62,7 +62,7 @@ def listing(request):
 
 def post_update(request,id =None):
 	instance = get_object_or_404(Post, id=id)
-	form = PostForm(request.POST or None , instance=instance)
+	form = PostForm(request.POST or None, request.FILES or None , instance=instance)
 	if form.is_valid():
 		instance = form.save(commit=False)
 		instance.save()
