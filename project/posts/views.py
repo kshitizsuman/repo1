d2004cbe4a7debd7e6c8ff38ef41b3ae153c8dba
Anubhav Shrivaastava <anubhavs@iitk.ac.in>
2016-11-04 20:20:37 +0530

    altered the visibility of draft and publish feature

--- project/posts/views.py
+++ project/posts/views.py
@@ -4,6 +4,7 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render
 from django.http import HttpResponse,HttpResponseRedirect,Http404
 from django.shortcuts import render , get_object_or_404,redirect
+from django.utils import timezone
 
 # Create your views here.
 from .forms import PostForm
@@ -25,9 +26,10 @@ def post_create(request):
 	return render(request,"post_form.html",context )
 
 def post_detail(request , slug=None):
-	if not request.user.is_staff or not request.user.is_superuser:
-		raise Http404
 	instance = get_object_or_404(Post, slug=slug)
+	if instance.draft or instance.publish > timezone.now().date():
+		if not request.user.is_staff or not request.user.is_superuser:
+			raise Http404
 	share_string = quote_plus(instance.content)
 	context ={
 		"title" : instance.title,
@@ -37,8 +39,10 @@ def post_detail(request , slug=None):
 	return render(request,"post_detail.html",context )
 
 def post_list(request):
-
-	queryset_list = Post.objects.all()
+	today = timezone.now().date()
+	queryset_list = Post.objects.active()
+	if request.user.is_staff or request.user.is_superuser:
+		query_list = Post.objects.all()
 	paginator = Paginator(queryset_list, 2) # Show 25 contacts per page
 	page_request_var = "page"
 	page = request.GET.get(page_request_var)
@@ -53,7 +57,8 @@ def post_list(request):
 	context ={
 		"object_list" : queryset,
 		"title" : "List",
-		"page_request_var": page_request_var
+		"page_request_var": page_request_var,
+		"today": today,
 	}
 	return render(request,"post_list.html",context )
 
