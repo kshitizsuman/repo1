cd2e582de221de640f0f529f9f94878a44682db0
Kshitiz Suman <kshitizsuman@gmail.com>
2016-11-07 03:56:59 +0530

    Created comments app

---
+++ project/comments/models.py
@@ -0,0 +1,23 @@
+from __future__ import unicode_literals
+
+
+from django.conf import settings
+from django.db import models
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
+
+from posts.models import Post
+
+# Create your models here.
+
+class Comment(models.Model):
+	user=models.ForeignKey(settings.AUTH_USER_MODEL,default=1) 
+	post =models.ForeignKey(Post)
+	content = models.TextField()
+	timestamp = models.DateTimeField(auto_now_add=True)
+
+	def __unicode__(self):
+		return str(self.user.username)
+
+	def __str__(self):
+		return str(self.user.username)
