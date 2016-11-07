5bd7e6cbef5420456128d073b8adb65624d1e020
Rishabh Bhardwaj <brishabh@iitk.ac.in>
2016-11-07 12:20:08 +0530

    linked pages with each other. added code for used authentication

--- project/accounts/views.py
+++ project/accounts/views.py
@@ -11,6 +11,9 @@ from .forms import UserLoginForm, UserRegisterForm
 
 def login_view(request):
     #print(request.user.is_authenticated())
+    domain="http://"+request.META['HTTP_HOST']
+    register=domain+"/register/"
+    loginlink =domain+'/login/' 
     next = request.GET.get('next')
     title = "Login"
     form = UserLoginForm(request.POST or None)
@@ -22,10 +25,13 @@ def login_view(request):
         if next:
             return redirect(next)
         return redirect("/")
-    return render(request, "form.html", {"form":form, "title": title})
+    return render(request, "form.html", {"message":"","form":form, "title": title , "register":register,"dashboard":domain,"loginlink":loginlink})
 
 
 def register_view(request):
+    domain="http://"+request.META['HTTP_HOST']
+    register=domain+"/register/"
+    loginlink =domain+'/login/'
     print(request.user.is_authenticated())
     next = request.GET.get('next')
     title = "Register"
@@ -42,6 +48,10 @@ def register_view(request):
         return redirect("/")
 
     context = {
+        "message":"",
+        "register":register,
+        "dashboard":domain,
+        "loginlink":loginlink,
         "form": form,
         "title": title
     }
@@ -49,6 +59,9 @@ def register_view(request):
 
 
 def logout_view(request):
+    domain="http://"+request.META['HTTP_HOST']
+    register=domain+"/register/"
+    loginlink =domain+'/login/'
     logout(request)
-    return render(request, "form.html", {})
+    return render(request, "form2.html", {"register":register,"dashboard":domain,"loginlink":loginlink})
     return redirect("/")
